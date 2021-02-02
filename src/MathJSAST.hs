28b3556a5a8b394f8d2fd6be6bcd9695809575b3
B Horn <b@horn.uk>
2021-02-02 03:11:49 +0100

    Supporting more of the language
    So, we have a pretty decent representation of the AST now, just need a
    nice way of converting a more natural language to it.
    
    Signed-off-by: B Horn <b@horn.uk>

--- src/MathJSAST.hs
+++ src/MathJSAST.hs
@@ -2,30 +2,70 @@
 
 module MathJSAST where
 
+import Data.List
+
 -- types we support
-data Types = String String
+data Types = Bool Bool 
+           | String String
            | Int Int
+           | Float Float
            deriving (Eq)
 
+-- Basic operators
+data Operators = Plus
+               | Minus
+               | Mul
+               | Div
+               | Power
+               deriving (Eq)
+
+data Variable = Variable String
+
 -- Our abstract Syntax
-data Expr = Seq Expr Expr       -- Sequence of expressions
-          | Var String          -- Variable
-          | Assign (Expr (Var)) Types   -- Assign a value to a variable.
+data Expr = Constant Types      -- Constant
+          | Seq Expr Expr       -- Sequence of expressions
+          | Var Variable        -- Variable
+          | Infix Operators Expr Expr
+                                -- Infix Operation
+          | Assign Variable Expr
+                                -- Assign a value to a variable.
           | If Expr Expr Expr   -- If expression
-          | Fun String [Expr] Expr
-                                -- Function, name, argument list, along with the body
-                                -- in which they are bound in.
+          | Fun String [Variable] Expr
+                                -- Function, name, argument list, along with the
+                                -- body in which they are bound in.
                                 -- don't believe we can use anonymous functions,
                                 -- so it needs a name.
+          | Call String [Expr]  -- Calls a function.
 
 instance Show Types where
+    show (Bool True) = "true"
+    show (Bool False) = "false"
+
     show (String x) =
         "\"" ++ x ++ "\""
+
     show (Int x) = show x
+    show (Float x) = show x
+
+instance Show Operators where
+    show x = case x of
+               Plus -> "+"
+               Minus -> "-"
+               Mul -> "*"
+               Div -> "/"
+               Power -> "^"
+
+instance Show Variable where
+    show (Variable x) =
+        x
 
 instance Show Expr where
+    show (Constant x) =
+        show x
     show (Var x) =
-        x
+        show x
+    show (Infix op s1 s2) =
+        "(" ++ show s1 ++ ")" ++ show op ++ "(" ++ show s2 ++ ")"
     show (Seq s1 s2) =
         show s1 ++ ";" ++ show s2
     show (Assign var value) =
@@ -33,4 +73,7 @@ instance Show Expr where
     show (If s s1 s2) =
         show s ++ "?" ++ show s1 ++ ":" ++ show s2
     show (Fun name args expr) =
-        name ++ "(" ++ show args ++ ")" ++ "=" ++ show expr
+        name ++ "(" ++ (intercalate "," $ map show args) ++ ")"
+        ++ "=" ++ show expr
+    show (Call name args) =
+        name ++ "(" ++ (intercalate "," $ map show args) ++ ")"
